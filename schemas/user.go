3484b3cff372dba9bda082d0f302915c8634a9ae
Henir4 <115803233+Henir4@users.noreply.github.com>
2026-08-08 18:28:50 -0300

    feat: add JWT support and user authentication structures

--- schemas/user.go
+++ schemas/user.go
@@ -1,11 +1,35 @@
 package schemas
 
-import "gorm.io/gorm"
+import (
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+	"gorm.io/gorm"
+)
+
+var jwtKey = []byte(os.Getenv("JWT"))
 
 type User struct {
   gorm.Model
 
   Username string
   Password string
-  Role string
+  Email    string
+}
+
+type UserResponse struct {
+  ID       uint   `json:"id"`
+  Username string `json:"username"`
+  Email    string `json:"email"`
+}
+
+type LoginRequest struct {
+  Username string `json:"username" binding:"required"`
+  Password string `json:"password" binding:"required"`
+}
+
+type Claims struct {
+  Username string `json:"username"`
+  UserID   string `json:"user_id"`
+  jwt.RegisteredClaims
 }
