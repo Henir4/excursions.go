8e0a8483892a1889ed49e808effae1d6c04014c8
Henir4 <115803233+Henir4@users.noreply.github.com>
2026-08-08 20:55:46 -0300

    style: improve code formatting and consistency in user.go

--- schemas/user.go
+++ schemas/user.go
@@ -1,37 +1,64 @@
 package schemas
 
 import (
+	"bytes"
 	"os"
 
 	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 var JwtKey = []byte(os.Getenv("JWT"))
 
 type User struct {
-  gorm.Model
+	gorm.Model
 
-  Username string
-  UserID   string
-  Password string
-  Email    string
+	Username string
+	UserID   string
+	Password string
+	Email    string
+}
+
+func (u *User) BeforeSave(tx *gorm.DB) (err error) {
+	// Check if the password is empty, if so, skip hashing
+	if u.Password == "" {
+		return nil
+	}
+
+	// Check if the password is already hashed
+	passwordBytes := []byte(u.Password)
+	if bytes.HasPrefix(passwordBytes, []byte("$2a$")) ||
+		bytes.HasPrefix(passwordBytes, []byte("$2b$")) ||
+		bytes.HasPrefix(passwordBytes, []byte("$2y$")) {
+		return nil
+	}
+
+	// Hash the password using bcrypt
+	hashedPassword, err := bcrypt.GenerateFromPassword(passwordBytes, bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	// Set the hashed password back to the User struct
+	u.Password = string(hashedPassword)
+	return nil
 }
 
 type UserResponse struct {
-  ID       uint   `json:"id"`
-  UserID   string `json:"user_id"`
-  Username string `json:"username"`
-  Email    string `json:"email"`
+	ID       uint   `json:"id"`
+	UserID   string `json:"user_id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
 }
 
 type LoginRequest struct {
-  Username string `json:"username" binding:"required"`
-  Password string `json:"password" binding:"required"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 type Claims struct {
-  Username string `json:"username"`
-  UserID   string `json:"user_id"`
-  jwt.RegisteredClaims
+	Username string `json:"username"`
+	UserID   string `json:"user_id"`
+	jwt.RegisteredClaims
 }
