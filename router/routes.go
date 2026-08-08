8cfa286aed609794d6385b88047228cbd912f6ff
Henir4 <115803233+Henir4@users.noreply.github.com>
2026-08-08 20:55:37 -0300

    style: reorganize route initialization for better structure and readability

--- router/routes.go
+++ router/routes.go
@@ -1,8 +1,8 @@
 package router
 
 import (
-
 	handler "excursion.com/handlers"
+	"excursion.com/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,5 +16,17 @@ func initializeroutes(router *gin.Engine) {
     v1.POST("/excursion", handler.CreateExcursionHandler)
     v1.DELETE("/excursion", handler.DeleteExcursionHandler)
     v1.PUT("/excursion", handler.UpdateExcursionHandler)
+  }
+
+  auth := router.Group("/api/v1/auth")
+  {
+    // auth.POST("/register", handler.RegisterHandler)
+    auth.POST("/login", handler.LoginHandler)
+  }
+
+  protected := router.Group("/api/v1")
+  protected.Use(middleware.AuthMiddleware())
+  {
+    protected.GET("/protected", handler.ProtectedHandler)
   }
 }
