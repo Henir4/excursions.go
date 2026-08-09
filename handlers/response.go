6a0ef4f8bdca91d760cfc681ebcd46bb315bae30
Henir4 <115803233+Henir4@users.noreply.github.com>
2026-08-09 17:43:26 -0300

    feat: add response handling functions and structures for API responses

--- handlers/response.go
+++ handlers/response.go
@@ -1,6 +1,11 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"excursion.com/schemas"
+	"github.com/gin-gonic/gin"
+)
 
 // sendError sends a JSON error response with the specified HTTP status code and message.
 func sendError(ctx *gin.Context, code int, msg string) {
@@ -9,4 +14,49 @@ func sendError(ctx *gin.Context, code int, msg string) {
 		"message": msg,
 		"errorCode": code,
 	})
+}
+
+// sendSuccess sends a JSON success response with the specified message and data.
+func sendSuccess(ctx *gin.Context, op string, data interface{}) {
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(http.StatusOK, gin.H {
+		"message": op,
+		"data": data,
+	})
+}
+
+// Response structures for various API responses
+type ErrorResponse struct {
+	Message string `json:"message"`
+	ErrorCode int `json:"errorCode"`
+}
+
+// CreateExcursionResponse represents the response structure for creating an excursion.
+type CreateExcursionResponse struct {
+	Message string `json:"message"`
+	Data schemas.ExcursionResponse `json:"data"`
+}
+
+// DeleteExcursionResponse represents the response structure for deleting an excursion.
+type DeleteExcursionResponse struct {
+	Message string `json:"message"`
+	Data schemas.ExcursionResponse `json:"data"`
+}
+
+// ShowExcursionResponse represents the response structure for showing a single excursion.
+type ShowExcursionResponse struct {
+	Message string `json:"message"`
+	Data schemas.ExcursionResponse `json:"data"`
+}
+
+// ShowExcursionsResponse represents the response structure for showing multiple excursions.
+type ShowExcursionsResponse struct {
+	Message string `json:"message"`
+	Data []schemas.ExcursionResponse `json:"data"`
+}
+
+// UpdateExcursionResponse represents the response structure for updating an excursion.
+type UpdateExcursionResponse struct {
+	Message string `json:"message"`
+	Data schemas.ExcursionResponse `json:"data"`
 }
